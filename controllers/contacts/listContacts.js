67af9b40a923747f3459135a18e797b1ac5cf6db
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-07-08 23:26:16 +0300

    Finalize register and login, add draft for updated contact controllers

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,11 @@ const { Contact } = require('../../models/contact');
 const { ctrlWrapper } = require('../../helpers');
 
 const listContacts = async (req, res) => {
-  const result = await Contact.find();
+  const { _id: owner } = req.user;
+  const result = await Contact.find(
+    { owner },
+    '-createdAt -updatedAt'
+  ).populate('owner', 'email subscription');
   res.json(result);
 };
 
